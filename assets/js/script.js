81b17231de5db3a5bda22d7bb565f326347287df
Nate Bhurinat Wangsutthitham <thepooe@gmail.com>
2017-04-14 17:52:34 +1000

    Add divider and smooth scroll

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(".navbar a[href^='#']").on('click', function(e) {
+    // prevent default anchor click behavior
+    e.preventDefault();
+    // animate
+    $('html, body').animate({
+        scrollTop: this.offsetTop
+    });
+});
