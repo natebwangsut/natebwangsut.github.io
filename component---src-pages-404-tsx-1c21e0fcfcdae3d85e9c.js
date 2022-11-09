23a903c86f667660179bc86f7e8aedd57cf180e0
natebwangsut <natebwangsut@users.noreply.github.com>
2022-11-09 05:16:21 +0000

    deployed via Gatsby Publish Action 🎩 for 9497073532f02c670f69e03a4fde567a523ebcaa

---
+++ component---src-pages-404-tsx-1c21e0fcfcdae3d85e9c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknatebwangsut_github_io=self.webpackChunknatebwangsut_github_io||[]).push([[218],{2513:function(e,t,n){n.r(t);var u=n(7294),a=n(1858),l=n(1992);t.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("div",{style:{marginTop:100}}),u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1c21e0fcfcdae3d85e9c.js.map
---
+++ component---src-pages-blog-tsx-29a67afd9734b7947932.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknatebwangsut_github_io=self.webpackChunknatebwangsut_github_io||[]).push([[410],{8645:function(e,t,n){n.r(t);var l=n(7294),a=n(1858),u=n(1992);t.default=function(){return l.createElement(a.Z,null,l.createElement(u.Z,{title:"Blog"}),l.createElement("div",{style:{marginTop:100}}),l.createElement("h1",null,"Random Blog"),l.createElement("p",null,"Blogs item here"))}}}]);
+//# sourceMappingURL=component---src-pages-blog-tsx-29a67afd9734b7947932.js.map
