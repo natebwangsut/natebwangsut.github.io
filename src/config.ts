1346461d8e4493b5120a626998365bf677b13486
natebwangsut <nate.bwangsut@gmail.com>
2023-01-24 02:10:40 +0700

    fix: update og images

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,8 @@ const config = {
   siteTitleAlt: "natebwangsut",                                                             // Alternative site title for SEO.
   siteTitleShort: "natebwangsut",                                                           // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   // siteLogo: "/logos/logo-1024.png",                                                      // Logo used for SEO and manifest.
-  siteImage: "/images/og-image.avif",                                                       // Default Open Graph image.
+  siteImage: "/images/banner.avif",                                                         // Default Open Graph image.
+  siteImageAlt: "/images/banner.jpg",                                                       // Alternative Open Graph image.
   siteUrl: "https://natebwangsut.github.io",                                                // Domain of your website without pathPrefix.
   pathPrefix: "/",                                                                          // Prefixes all links.
   siteDescription: "Just another software engineer living on the same planet as you do.",   // Website description used for RSS feeds/meta description tag.
@@ -18,6 +19,10 @@ const config = {
   // theme: "--geek-blue",                                                                   // please see palette.css for more options
   // theme: "--mint",                                                                        // please see palette.css for more options
   // theme: "--veri-peri",                                                                   // please see palette.css for more options
+
+  twitter: {
+    handle: "@natebwangsut",
+  },
 
   // External links
   links: {
